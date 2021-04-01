50bd81ba621fe8f2a7a854c256dbc51fc8245e7b
laithfayizabedelrahimhussein <laithalsanory9919@gmail.com>
2021-04-01 16:51:13 +0300

    make a new file in js

---
+++ app.js
@@ -0,0 +1,14 @@
+alert("welcome to my page");
+
+ var math = prompt('please inter your age' ,'18');
+
+ var username = 'laith';
+
+ document.write (username);
+
+ if ( math >= 18 ) {
+     alrt ('u are welcome here ');
+    }
+    else {
+        alert ('this page only for more then 18');
+    }
